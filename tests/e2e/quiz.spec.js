671ee26d51e30259be04c76907f58cc6bc95e7ea
Strawberry91592 <w8hmz81kq7n2gw@hotmail.com>
2026-09-08 15:06:59 +0200

    Fix Playwright ESM test import

--- tests/e2e/quiz.spec.js
+++ tests/e2e/quiz.spec.js
@@ -1,4 +1,4 @@
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
 async function mockStats(page, totals = {}) {
   await page.route('**/stats', async route => {
